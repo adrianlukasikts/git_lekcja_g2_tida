3fe4c432ebc07d3107a441c48b97563e7b89516b
NauczycielWarszawaTe <adrian.lukasik@technischools.com>
2026-05-08 11:18:21 +0200

    added test print

--- main.py
+++ main.py
@@ -7,10 +7,11 @@
 def print_hello(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f"test")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hello('Test')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
